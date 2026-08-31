eb144a7ed597a7c206f6282e34a6e67004bfa2a5
24fikoba <24fi047@ms.dendai.ac.jp>
2026-08-31 17:03:29 +0900

    add: コメントを追加

--- server/handlers/game-handler.js
+++ server/handlers/game-handler.js
@@ -12,18 +12,22 @@ function handleStartGame({
   playerRooms,
   broadcastToRoom
 }) {
+  // プレイヤーIDが存在しない場合は処理しない
   if (!socket.playerId) {
     return;
   }
 
+  // プレイヤー情報を取得
   const playerId = socket.playerId;
   const roomId = playerRooms[playerId];
   const room = getRoom(roomId);
 
+  // ルームが存在しない場合は処理しない
   if (!room) {
     return;
   }
 
+  // 2人未満の場合はゲームを開始できない
   if (room.players.length < 2) {
     socket.send(JSON.stringify({
       type: "error",
@@ -33,6 +37,7 @@ function handleStartGame({
     return;
   }
 
+  // すでにゲームが開始されている場合は処理しない
   if (room.gameStarted || room.gameStarting) {
     return;
   }
@@ -44,12 +49,14 @@ function handleStartGame({
     roomId
   );
 
+  // 順番決定用のデータを初期化
   room.orderRolls = {};
   room.orderRollResults = {};
   room.orderRollGroups = [];
   room.orderRollCurrentGroup = [];
   room.orderRollRerolling = false;
 
+  // 現在のゲーム状態を全プレイヤーに通知
   broadcastToRoom(roomId, {
     type: "game_state",
     state: {
@@ -62,6 +69,7 @@ function handleStartGame({
     }
   });
 
+  // 順番決定ルーレットの開始を全プレイヤーに通知
   broadcastToRoom(roomId, {
     type: "order_roll_start"
   });
