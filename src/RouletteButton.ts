edbff7d31ee107fa5f580853f8b5e76f43837206
miyong00 <miomame.17@gmail.com>
2026-09-13 20:53:37 +0900

    fix: クリックしたことを知らせる

--- src/RouletteButton.ts
+++ src/RouletteButton.ts
@@ -1,22 +1,23 @@
-import { Roulette } from "./Roulette";
-
 export class RouletteButton{
     private element: HTMLButtonElement;
-    constructor(roulette: Roulette){
+
+    constructor(onClick: () => void) {
         this.element = document.createElement("button");
         this.element.id = "roulette-button";
         this.element.textContent = "ルーレットを回す";
+
         const gameContainer = document.getElementById("game-container");
-        if(gameContainer === null){
+
+        if (gameContainer === null) {
             throw new Error("game-containerが見つかりません");
         }
+
         gameContainer.appendChild(this.element);
-        this.element.addEventListener("click", ()=>{
-            if(roulette.getIsRolling()){
-                return;
-            }
-            // roulette.roll()
-            
-        });
+
+        this.element.addEventListener("click", onClick);
+    }
+
+    public setDisabled(disabled: boolean): void {
+        this.element.disabled = disabled;
     }
 }
